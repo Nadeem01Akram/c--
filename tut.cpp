90f1fa1b8c6cd7b7f714d1fa094125058891ee66
Nadeem Akram <024572nadeemakram@gmail.com>
2023-09-16 04:21:48 -0700

    first commit

---
+++ tut.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int number;
+
+    cout << "Enter number : ";
+    cin >> i;
+
+    for (; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+    cin >> number;
+    return 0;
+}
